68ef140b0c36c7e523156df911a1744eeb112bd8
takuya <55338+takuya@users.noreply.github.com>
2025-02-21 14:33:05 +0900

    test

--- tests/Units/Archive7zListSupportedTest.php
+++ tests/Units/Archive7zListSupportedTest.php
@@ -9,6 +9,7 @@ class Archive7zListSupportedTest extends TestCase {
 
   public function test_list_supported(){
     $ret = Archive7zWrapper::extensions();
+    dump($ret);
     $this->assertContains('zip',$ret);
     $this->assertContains('tar',$ret);
     $this->assertContains('7z',$ret);
